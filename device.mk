d2184f94b82f81ce0883e33b402afd4c4b834316
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-10-30 00:45:56 +0530

    RMX1805: change screen density to 384

--- device.mk
+++ device.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 #Soundrecorder
 PRODUCT_PACKAGES += \
